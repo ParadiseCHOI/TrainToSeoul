0639fe3f9f1c04de05ebd8be3241ffa988c865e5
ParadiseCHOI <cnw529@naver.com>
2021-06-13 23:16:55 +0900

    Scripts

---
+++ SoundSetting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundSetting : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider volume;
+
+    void Start()
+    {
+        volume.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+    }
+
+    public void SetLevel(){
+        float sliderValue = volume.value;
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+}
